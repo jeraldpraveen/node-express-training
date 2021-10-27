d1c17fbc5182f7cc7bde0d0bd1250ad1b359e7d9
jeraldpraveen <jerald.vit@gmail.com>
2021-10-27 20:24:50 +0530

    7.npm

---
+++ 01-node-tutorial/13-npm.js
@@ -0,0 +1,8 @@
+// npm - node package manager, comes with node
+// npm --version
+
+//local dependency - use it only in current project
+// npm i <packageName>
+
+// Global dependency - use it in any project
+// npm i -g <packageName>
