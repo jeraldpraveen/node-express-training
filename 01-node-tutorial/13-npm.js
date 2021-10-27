c4e9f2dcb164d3987cf00bfa5c362847680d20bd
jeraldpraveen <jerald.vit@gmail.com>
2021-10-27 20:46:15 +0530

    8.First Package

--- 01-node-tutorial/13-npm.js
+++ 01-node-tutorial/13-npm.js
@@ -6,3 +6,16 @@
 
 // Global dependency - use it in any project
 // npm i -g <packageName>
+
+// package.json - manuifest dependencies(stores important info about project/package)
+// manual approach (create package.json in root, create properties,etc)
+// npm init (step by step, press Enter to skip)
+// npm init -y (everything default)
+
+// npm i validator
+// npm i lodash
+
+const lodash = require("lodash");
+const items = [1, [2, [3, [4]]]];
+const oneArray = lodash.flattenDeep(items);
+console.log(oneArray);
