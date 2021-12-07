3f69ec38103f6ba433d72c119b1f5d134f2704af
jeraldpraveen <jerald.vit@gmail.com>
2021-12-08 04:23:09 +0530

    18.Http Drawback

---
+++ 02-express-tutorial/02-http-app.js
@@ -0,0 +1,40 @@
+console.log("Welcome To Express");
+
+const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method)
+  const url = req.url;
+  console.log(url);
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  }
+  // about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
+  // styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
+  }
+});
+
+server.listen(9000);
