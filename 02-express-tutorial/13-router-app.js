c20929a41d80ecf83737489892861a3e4cc85e26
jeraldpraveen <jerald.vit@gmail.com>
2021-12-12 16:11:52 +0530

    34.Express Router Example

--- 02-express-tutorial/13-router-app.js
+++ 02-express-tutorial/13-router-app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const people = require("./routes/people");
+const auth = require("./routes/auth");
 
 app.use(express.static("./files-for-request-methods"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/people", people);
+app.use("/login", auth);
 
 app.listen(5000);
