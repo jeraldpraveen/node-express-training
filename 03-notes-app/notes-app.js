caf07a3bd6823ff4ee4aa8452e4bc290af0b6521
jeraldpraveen <jerald.vit@gmail.com>
2021-12-17 05:53:41 +0530

    38.Drawback of [[process.argv]]

--- 03-notes-app/notes-app.js
+++ 03-notes-app/notes-app.js
@@ -4,3 +4,13 @@ const msg = getNotes();
 console.log(msg);
 
 console.log(process.argv);
+
+const command = process.argv[2];
+if (command === "add") {
+  console.log("Adding Note");
+} else {
+  console.log("Remove Note");
+}
+
+// In Terminal run [[node app.js add --title='This is my title']]
+// process.argv has to be sliced for --title ---> COMPLEX PROCESS
