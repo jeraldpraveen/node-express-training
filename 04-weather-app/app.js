f9a84d3d88e0ae5fdee241aa651452726d96be65
jeraldpraveen <jerald.vit@gmail.com>
2021-12-21 22:57:00 +0530

    52.Customizing http requests

--- 04-weather-app/app.js
+++ 04-weather-app/app.js
@@ -1,9 +1,10 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=2de870cf5fdc6af6c32dd9e861bc58d8&query=11.1085,77.3411";
+  "http://api.weatherstack.com/current?access_key=2de870cf5fdc6af6c32dd9e861bc58d8&query=11.1085,77.3411&units=f";
 
-request({ url: url }, (errorr, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (errorr, response) => {
+  const data = response.body.current;
+  const testWork = `It is currently ${data.temperature} degrees out. But it feels like ${data.feelslike} degrees out.`;
+  console.log(testWork);
 });
